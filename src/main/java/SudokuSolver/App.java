a884dddc47c748924eec100dd9728ffa77c05300
Thom Breugelmans <thombreugelmans@outlook.com>
2022-03-16 20:56:58 +0100

    Added TODO and removed 'hello world'

--- src/main/java/SudokuSolver/App.java
+++ src/main/java/SudokuSolver/App.java
@@ -4,6 +4,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        // TODO: link with sudoku solvers to create T/GUI for this application
     }
 }
